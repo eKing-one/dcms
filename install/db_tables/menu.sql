681db49f333f539c0d139f46d5c064d072616af3
skyour <skyour@foxmail.com>
2023-04-12 09:44:22 +0800

    小更新

--- install/db_tables/menu.sql
+++ install/db_tables/menu.sql
@@ -11,19 +11,18 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `icon` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `pos` (`pos`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='主菜单' AUTO_INCREMENT=15 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='首页菜单' AUTO_INCREMENT=15 ;
 --
 -- Дамп данных таблицы `menu`
 --
 INSERT INTO `menu` (`id`, `type`, `name`, `url`, `counter`, `pos`, `icon`) VALUES
 (1, 'link', '新闻中心', '/news/', 'news/count.php', 1, 'news.png'),
-(2, 'link', '聊天', '/chat/', 'chat/count.php', 7, 'chat.png'),
-(4, 'link', '留言板', '/guest/', 'guest/count.php', 9, 'guest.png'),
+(2, 'link', '在线聊天', '/chat/', 'chat/count.php', 7, 'chat.png'),
+(4, 'link', '在线留言', '/guest/', 'guest/count.php', 9, 'guest.png'),
 (5, 'link', '下载中心', '/down/', 'down/count.php', 5, 'down.png'),
-(6, 'link', '论坛', '/forum/', 'forum/count.php', 6, 'forum.png'),
-(7, 'link', '照片廊', '/photo/', 'photo/count.php', 10, 'photo.png'),
-(11, 'link', '领导者', '/user/liders/', '/user/liders/count.php', 4, 'lider.gif'),
-(10, 'link', '日记', '/plugins/notes/', 'plugins/notes/count.php', 8, 'zametki.gif'),
-(12, 'link', '熟人', '/user/love/', '/user/love/count.php', 3, 'meets.gif'),
-(13, 'link', '资料', '/plugins/rules/', '', 12, 'info.gif'),
-(14, 'link', '居民', '/user/users.php', '/user/count.php', 11, 'druzya.png');
+(6, 'link', '网站论坛', '/forum/', 'forum/count.php', 6, 'forum.png'),
+(7, 'link', '照片分享', '/photo/', 'photo/count.php', 10, 'photo.png'),
+(11, 'link', '网站领袖', '/user/liders/', '/user/liders/count.php', 4, 'lider.gif'),
+(10, 'link', '用户日记', '/plugins/notes/', 'plugins/notes/count.php', 8, 'zametki.gif'),
+(13, 'link', '网站资料', '/plugins/rules/', '', 12, 'info.gif'),
+(14, 'link', '网站居民', '/user/users.php', '/user/count.php', 11, 'druzya.png');
