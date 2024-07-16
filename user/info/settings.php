ca9a70d54f277f1ffa31c22803bb99513105296b
Citruy_404 <115987096+zzyh1145@users.noreply.github.com>
2024-07-17 00:37:01 +0800

    纠正错误翻译
    将电子邮件通知功能删除,不可用
    调整,补充说明一些设置项翻译

--- user/info/settings.php
+++ user/info/settings.php
@@ -16,7 +16,7 @@ if (isset($_POST['save'])) {
     if (isset($_POST['add_konts']) && ($_POST['add_konts'] == 2 || $_POST['add_konts'] == 1 || $_POST['add_konts'] == 0)) {
         $user['add_konts'] = intval($_POST['add_konts']);
         dbquery("UPDATE `user` SET `add_konts` = '$user[add_konts]' WHERE `id` = '$user[id]' LIMIT 1");
-    } else $err = '添加联系人模式错误';
+    } else $err = '添加联系人错误';
     if (isset($_POST['set_files']) && ($_POST['set_files'] == 1 || $_POST['set_files'] == 0)) {
         $user['set_files'] = intval($_POST['set_files']);
         dbquery("UPDATE `user` SET `set_files` = '$user[set_files]' WHERE `id` = '$user[id]' LIMIT 1");
@@ -67,10 +67,10 @@ err();
 aut();
 echo "<div id='comments' class='menus'>";
 echo "<div class='webmenu'>";
-echo "<a href='/user/info/settings.php' class='activ'>普通</a>";
+echo "<a href='/user/info/settings.php' class='activ'>通用</a>";
 echo "</div>";
 echo "<div class='webmenu last'>";
-echo "<a href='/user/tape/settings.php'>信息中心</a>";
+echo "<a href='/user/tape/settings.php'>通知消息</a>";
 echo "</div>";
 echo "<div class='webmenu last'>";
 echo "<a href='/user/discussions/settings.php'>讨论</a>";
@@ -79,16 +79,16 @@ echo "<div class='webmenu last'>";
 echo "<a href='/user/notification/settings.php'>通知书</a>";
 echo "</div>";
 echo "<div class='webmenu last'>";
-echo "<a href='/user/info/settings.privacy.php' >私隐保护</a>";
+echo "<a href='/user/info/settings.privacy.php' >隐私设置</a>";
 echo "</div>";
 echo "<div class='webmenu last'>";
-echo "<a href='/user/info/secure.php' >密码</a>";
+echo "<a href='/user/info/secure.php' >密码设置</a>";
 echo "</div>";
 echo "</div>";
 echo "<form method='post' action='?$passgen'>";
-echo "聊天中自动更新:<br /><input type='text' name='set_time_chat' value='$set[time_chat]' maxlength='3' /><br />";
-echo "每页显示:<br /><input type='text' name='set_p_str' value='$set[p_str]' maxlength='3' /><br />";
-echo "主题 (" . ($webbrowser ? 'WEB' : 'WAP') . "):<br /><select name='set_them" . ($webbrowser ? '2' : null) . "'>";
+echo "聊天室中自动刷新(秒):<br /><input type='text' name='set_time_chat' value='$set[time_chat]' maxlength='3' /><br />";
+echo "每页显示信息条数:<br /><input type='text' name='set_p_str' value='$set[p_str]' maxlength='3' /><br />";
+echo "切换主题(" . ($webbrowser ? 'WEB' : 'WAP') . "):<br /><select name='set_them" . ($webbrowser ? '2' : null) . "'>";
 $opendirthem = opendir(H . 'style/themes');
 while ($themes = readdir($opendirthem)) {
     // пропускаем корневые папки и файлы
@@ -99,33 +99,31 @@ while ($themes = readdir($opendirthem)) {
 }
 closedir($opendirthem);
 echo "</select><br />";
-echo "上传文件:<br /><select name='set_files'>";
-echo "<option value='1'" . ($user['set_files'] == 1 ? " selected='selected'" : null) . ">展场</option>";
-echo "<option value='0'" . ($user['set_files'] == 0 ? " selected='selected'" : null) . ">否定使用上传</option>";
+echo "论坛上传文件:<br /><select name='set_files'>";
+echo "<option value='1'" . ($user['set_files'] == 1 ? " selected='selected'" : null) . ">开启</option>";
+echo "<option value='0'" . ($user['set_files'] == 0 ? " selected='selected'" : null) . ">关闭</option>";
 echo "</select><br />";
-echo "地点:<br /><select name='show_url'>";
+echo "个人资料地点:<br /><select name='show_url'>";
 echo "<option value='1'" . ($user['show_url'] == 1 ? " selected='selected'" : null) . ">显示</option>";
 echo "<option value='0'" . ($user['show_url'] == 0 ? " selected='selected'" : null) . ">隐藏</option>";
 echo "</select><br />";
-echo "添加联系人:<br /><select name='add_konts'>";
-echo "<option value='2'" . ($user['add_konts'] == 2 ? " selected='selected'" : null) . ">阅读讯息时</option>";
-echo "<option value='1'" . ($user['add_konts'] == 1 ? " selected='selected'" : null) . ">写消息时</option>";
-echo "<option value='0'" . ($user['add_konts'] == 0 ? " selected='selected'" : null) . ">只能手动操作</option>";
+echo "自动添加联系人:<br /><select name='add_konts'>";
+echo "<option value='2'" . ($user['add_konts'] == 2 ? " selected='selected'" : null) . ">阅读信息后</option>";
+echo "<option value='1'" . ($user['add_konts'] == 1 ? " selected='selected'" : null) . ">互发消息后</option>";
+echo "<option value='0'" . ($user['add_konts'] == 0 ? " selected='selected'" : null) . ">手动添加</option>";
 echo "</select><br />";
 echo "时间<br /><select name=\"set_timesdvig\"><br />";
 for ($i = -12; $i < 12; $i++) {
     echo "<option value='$i'" . ($user['set_timesdvig'] == $i ? " selected='selected'" : null) . ">" . date("G:i", $time + $i * 60 * 60) . "</option>";
 }
 echo "</select><br />";
-if ($user['ank_mail'])
-    echo "<label><input type='checkbox' name='set_news_to_mail'" . ($user['set_news_to_mail'] ? " checked='checked'" : null) . " value='1' /> 通过电子邮件接收新闻</label><br />";
 echo "显示色情材料没有警告:<br />";
 echo "<input name='metka'" . ($user['abuld'] == 0 ? " checked='checked'" : null) . "  type='radio' value='0' />开 ";
 echo "<input name='metka'" . ($user['abuld'] == 1 ? " checked='checked'" : null) . "  type='radio' value='1' />关<br />";
-echo "<input type='submit' name='save' value='保存' />";
+echo "<input type='submit' name='save' value='保存设置' />";
 echo "</form>";
 echo "<div class=\"foot\">";
 echo "<img src='/style/icons/str2.gif' alt='*'> " . user::nick($user['id']) . " | ";
-echo '<b>普通</b>';
+echo '<b>通用</b>';
 echo "</div>";
 include_once '../../sys/inc/tfoot.php';
