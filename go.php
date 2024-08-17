af743f60308f70338b7f526f0b2e4ed8e5bdc1e8
zzyh1145 <windrs9@outlook.com>
2024-08-18 00:07:46 +0800

    更改提示

--- go.php
+++ go.php
@@ -8,7 +8,7 @@ include_once 'sys/inc/db_connect.php';
 include_once 'sys/inc/ipua.php';
 include_once 'sys/inc/fnc.php';
 include_once 'sys/inc/user.php';
-$set['title']='外部链接跳转提';
+$set['title']='外部链接跳转提示';
 include_once 'sys/inc/thead.php';
 title();
 if (!isset($_GET['go']) || (dbresult(dbquery("SELECT COUNT(*) FROM `rekl` WHERE `id` = '".intval($_GET['go'])."'"),0)==0 && !preg_match('#^https?://#',@base64_decode($_GET['go']))))
@@ -29,8 +29,8 @@ dbquery("UPDATE `rekl` SET `count` = '".($rekl['count']+1)."' WHERE `id` = '$rek
 if (isset($_SESSION['adm_auth']))unset($_SESSION['adm_auth']);
 header("Refresh: 2; url=$rekl[link]");
 echo "外部链接跳转提示<br />
-你点击了不属于本站的链接,
-本站不保证链接的安全性，请谨慎访问，防止感染病毒或上当。<br />";
+你点击了不属于本站的链接,点击后会使你离开本站
+本站不保证链接的安全性，请谨慎访问，防止感染病毒或上当受骗。<br />";
 echo "你访问的链接是：<b><a href=\"$rekl[link]\">$rekl[link]</a></b><br />";
 echo "访问次数: $rekl[count]<br />";
 }
