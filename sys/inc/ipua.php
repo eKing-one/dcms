fb83e69f29d5f401bd0658e56a034255ddb03c03
zzyh1145 <windrs9@outlook.com>
2024-08-26 09:34:06 +0800

    fix:回滚旧版本

--- sys/inc/ipua.php
+++ sys/inc/ipua.php
@@ -17,19 +17,21 @@ if(isset($_SERVER['REMOTE_ADDR']) && preg_match("#^([0-9]{1,3})\.([0-9]{1,3})\.(
 }
 $ip = $ipa[0];
 $iplong = ip2long($ip);
-if (isset($_SERVER['HTTP_USER_AGENT'])) {
-    $ua = $_SERVER['HTTP_USER_AGENT'];
-    // 清理 UA 字符串，保留有用的信息
-    $ua = preg_replace('/[^a-zA-Z0-9\/\.\-\s]/', '', $ua);
-    // 特殊处理 Opera Mini
-    if (isset($_SERVER['HTTP_X_OPERAMINI_PHONE_UA']) && preg_match('#Opera#i', $ua)) {
-        $ua_om = $_SERVER['HTTP_X_OPERAMINI_PHONE_UA'];
-        $ua_om = trim(strtok($ua_om, '/'));
-        $ua_om = trim(strtok('', '('));
-        $ua_om = preg_replace('/[^a-zA-Z0-9\/\.\-\s]/', '', $ua_om);
-        $ua = "Opera Mini ($ua_om)";
-    }
-} else {
-    $ua = 'unknown';  // 设置一个更标准的默认 UA 字符串
+if (isset($_SERVER['HTTP_USER_AGENT']))
+{
+	$ua = $_SERVER['HTTP_USER_AGENT'];
+	$ua = strtok($ua, '/');
+	$ua = strtok($ua, '('); // 我们只留下括号前的内容
+	$ua = preg_replace('#[^a-z_\./ 0-9\-]#iu',"null", $ua); // 我们剪掉了所有的"左"字符
+	// Opera mini还会发送有关手机的数据 :)
+	if (isset($_SERVER['HTTP_X_OPERAMINI_PHONE_UA']) && preg_match('#Opera#i',$ua))
+	{
+		$ua_om = $_SERVER['HTTP_X_OPERAMINI_PHONE_UA'];
+		$ua_om = strtok($ua_om, '/');
+		$ua_om = strtok($ua_om, '(');
+		$ua_om = preg_replace('#[^a-z_\. 0-9\-]#iu', "null", $ua_om);
+		$ua = 'Opera Mini ('.$ua_om.')';
+	}
 }
+else $ua = '没有可用的数据';
 ?>
