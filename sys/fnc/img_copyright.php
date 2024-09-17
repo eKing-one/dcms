cdd691a99152efb207587410ef032e71f2262717
guguan123 <guguanming@outlook.com>
2024-09-17 17:03:46 +0800

    回滚关于删除图片水印的代码

---
+++ sys/fnc/img_copyright.php
@@ -0,0 +1,28 @@
+<?
+// 我们对图像施加版权，如果有的话。
+// 版权大小根据图像动态变化
+function img_copyright($img) {
+	global $set;
+	if (isset($set['copy_path']) && $set['copy_path'] != null && $copy = @imagecreatefromstring(file_get_contents(H.$set['copy_path']))) {
+		$img_x = imagesx($img);
+		$img_y = imagesy($img);
+		$copy_x = imagesx($copy);
+		$copy_y = imagesy($copy);
+		$w = intval(min($img_x / 2.5, $copy_x, 128));
+		$h = intval(min($img_y / 2.5, $copy_y, 64));
+		$x_ratio = $w / $copy_x;
+		$y_ratio = $h / $copy_y;
+		if (($copy_x <= $w) && ($img_y <= $h)) {
+			$dstW = $copy_x;
+			$dstH = $copy_y;
+		} elseif (($x_ratio * $copy_y) < $h) {
+			$dstH = ceil($x_ratio * $copy_y);
+			$dstW = $w;
+		} else {
+			$dstW = ceil($y_ratio * $copy_x);
+			$dstH = $h;
+		}
+		imagecopyresampled($img, $copy, $img_x-$dstW, $img_y - $dstH, 0, 0, $dstW, $dstH, $copy_x, $copy_y);
+	}
+	return $img;
+}
