1a211f7b65259648501dcce88d105e4044c2f324
guguan123 <guguanming@outlook.com>
2024-10-07 01:59:17 +0800

    改进代码风格，提高可读性

--- user/aut.php
+++ user/aut.php
@@ -32,13 +32,21 @@ aut();
 if ((!isset($_SESSION['refer']) || $_SESSION['refer'] == NULL) && isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] != NULL && !preg_match('#mail\.php#', $_SERVER['HTTP_REFERER'])) {
 	$_SESSION['refer'] = str_replace('&', '&amp;', preg_replace('#^http://[^/]*/#', '/', $_SERVER['HTTP_REFERER']));
 }
-echo '<form class="mess" method="post" action="/user/login.php">
-账号:<br /><input type="text" name="login" maxlength="32" /><br />
-密码:<br /><input type="password" name="pass" maxlength="32" /><br />
-<label><input type="checkbox" name="aut_save" value="1" />记住我</label><br />
-<input type="submit" value="登录" />
-</form>
-<div class="foot">尚未登记？ <br /><a href="/user/reg.php">注册账号</a><br /></div>
-<div class="foot">忘记密码？<br /><a href="/user/pass.php">密码恢复</a><br /></div>';
+?>
 
-include_once '../sys/inc/tfoot.php';
+<form class="mess" method="post" action="/user/login.php">
+	账号:<br />
+	<input type="text" name="login" maxlength="32" /><br />
+	密码:<br />
+	<input type="password" name="pass" maxlength="32" /><br />
+	<label><input type="checkbox" name="aut_save" value="1" />记住我</label><br />
+	<input type="submit" value="登录" />
+</form>
+<div class="foot">尚未登记？ <br />
+	<a href="/user/reg.php">注册账号</a><br />
+</div>
+<div class="foot">忘记密码？<br />
+	<a href="/user/pass.php">密码恢复</a><br />
+</div>
+
+<?php include_once '../sys/inc/tfoot.php'; ?>
