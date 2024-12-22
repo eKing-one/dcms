537693d17d251feee53ad56afc410666acaf61a7
guguan123 <guguanming@outlook.com>
2024-12-22 16:36:22 +0800

    格式化安装向导的代码

--- install/inc/functions.php
+++ install/inc/functions.php
@@ -1,103 +1,102 @@
 <?php
-function br($msg, $br = '<br />')
-{
-    return preg_replace("#((<br( ?/?)>)|\n|\r)+#i", $br, $msg);
+function br($msg, $br = '<br />') {
+	return preg_replace("#((<br( ?/?)>)|\n|\r)+#i", $br, $msg);
 } // 换行符
-function my_esc($text, $br = NULL)
-{ // 剪切所有不可读字符
-    if ($br != NULL)
-        for ($i = 0; $i <= 31; $i++) $text = str_replace(chr($i), NULL, $text);
-    else {
-        for ($i = 0; $i < 10; $i++) $text = str_replace(chr($i), NULL, $text);
-        for ($i = 11; $i < 20; $i++) $text = str_replace(chr($i), NULL, $text);
-        for ($i = 21; $i <= 31; $i++) $text = str_replace(chr($i), NULL, $text);
-    }
-    return $text;
+
+function my_esc($text, $br = NULL) { // 剪切所有不可读字符
+	if ($br != NULL) {
+		for ($i = 0; $i <= 31; $i++) $text = str_replace(chr($i), NULL, $text);
+	} else {
+		for ($i = 0; $i < 10; $i++) $text = str_replace(chr($i), NULL, $text);
+		for ($i = 11; $i < 20; $i++) $text = str_replace(chr($i), NULL, $text);
+		for ($i = 21; $i <= 31; $i++) $text = str_replace(chr($i), NULL, $text);
+	}
+	return $text;
 }
-function output_text($str, $br = true, $html = true, $smiles = true, $links = true, $bbcode = true)
-{
-    if ($html == true)
-        $str = htmlentities($str, ENT_QUOTES, 'UTF-8'); // 将所有操作转换为正常的浏览器消化
-    if ($br == true) {
-        $str = br($str); // 换行符
-        $str = my_esc($str); // 我们删除了所有无法读取的字符，这些字符会破坏我们的标记:)
-    } else {
-        //$str=br($str, ' '); // 空格代替进位
-        $str = my_esc($str); // 我们删除了所有无法读取的字符，这些字符会破坏我们的标记:)
-    }
-    return $str; // 返回已处理的字符串
+
+function output_text($str, $br = true, $html = true, $smiles = true, $links = true, $bbcode = true) {
+	if ($html == true) {
+		$str = htmlentities($str, ENT_QUOTES, 'UTF-8'); // 将所有操作转换为正常的浏览器消化
+	}
+	if ($br == true) {
+		$str = br($str); // 换行符
+		$str = my_esc($str); // 我们删除了所有无法读取的字符，这些字符会破坏我们的标记:)
+	} else {
+		//$str=br($str, ' '); // 空格代替进位
+		$str = my_esc($str); // 我们删除了所有无法读取的字符，这些字符会破坏我们的标记:)
+	}
+	return $str; // 返回已处理的字符串
 }
-function msg($msg)
-{
-    echo '<div class="msg">' . $msg . '</div>';
+
+function msg($msg) {
+	echo '<div class="msg">' . $msg . '</div>';
 } // 消息输出
 
-function passgen($k_simb = 8, $types = 3)
-{
-    $password = "";
-    $small = "abcdefghijklmnopqrstuvwxyz";
-    $large = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    $numbers = "1234567890";
-    mt_srand((float)microtime() * 1000000);
-    for ($i = 0; $i < $k_simb; $i++) {
-        $type = mt_rand(1, min($types, 3));
-        switch ($type) {
-            case 3:
-                $password .= $large[mt_rand(0, 25)];
-                break;
-            case 2:
-                $password .= $small[mt_rand(0, 25)];
-                break;
-            case 1:
-                $password .= $numbers[mt_rand(0, 9)];
-                break;
-        }
-    }
-    return $password;
+function passgen($k_simb = 8, $types = 3) {
+	$password = "";
+	$small = "abcdefghijklmnopqrstuvwxyz";
+	$large = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	$numbers = "1234567890";
+	mt_srand((float)microtime() * 1000000);
+	for ($i = 0; $i < $k_simb; $i++) {
+		$type = mt_rand(1, min($types, 3));
+		switch ($type) {
+			case 3:
+				$password .= $large[mt_rand(0, 25)];
+				break;
+			case 2:
+				$password .= $small[mt_rand(0, 25)];
+				break;
+			case 1:
+				$password .= $numbers[mt_rand(0, 9)];
+				break;
+		}
+	}
+	return $password;
 }
 $passgen = passgen();
+
 // 保存系统设置
-function save_settings($set)
-{
-    // 从数组中移除特定键
-    unset($set['web']);
-    
-    // 构建配置文件内容
-    $configContent = "<?php\nreturn " . var_export($set, true) . ";\n";
+function save_settings($set) {
+	// 从数组中移除特定键
+	unset($set['web']);
+	
+	// 构建配置文件内容
+	$configContent = "<?php\nreturn " . var_export($set, true) . ";\n";
 
-    // 定义配置文件路径
-    $filePath = H . 'sys/dat/settings.php';
+	// 定义配置文件路径
+	$filePath = H . 'sys/dat/settings.php';
 
-    // 尝试打开文件写入内容
-    if ($fopen = @fopen($filePath, 'w')) {
-        @fputs($fopen, $configContent);
-        @fclose($fopen);
-        @chmod($filePath, 0777);
-        return true;
-    } else {
-        return false;
-    }
+	// 尝试打开文件写入内容
+	if ($fopen = @fopen($filePath, 'w')) {
+		@fputs($fopen, $configContent);
+		@fclose($fopen);
+		@chmod($filePath, 0777);
+		return true;
+	} else {
+		return false;
+	}
 }
+
 // 递归删除文件夹
-function delete_dir($dir)
-{
-    if (is_dir($dir)) {
-        $od = opendir($dir);
-        while ($rd = readdir($od)) {
-            if ($rd == '.' || $rd == '..') continue;
-            if (is_dir("$dir/$rd")) {
-                @chmod("$dir/$rd", 0777);
-                delete_dir("$dir/$rd");
-            } else {
-                @chmod("$dir/$rd", 0777);
-                @unlink("$dir/$rd");
-            }
-        }
-        closedir($od);
-        @chmod("$dir", 0777);
-        return @rmdir("$dir");
-    } else {
-        @chmod("$dir", 0777);
-        @unlink("$dir");
-    }
+function delete_dir($dir) {
+	if (is_dir($dir)) {
+		$od = opendir($dir);
+		while ($rd = readdir($od)) {
+			if ($rd == '.' || $rd == '..') continue;
+			if (is_dir("$dir/$rd")) {
+				@chmod("$dir/$rd", 0777);
+				delete_dir("$dir/$rd");
+			} else {
+				@chmod("$dir/$rd", 0777);
+				@unlink("$dir/$rd");
+			}
+		}
+		closedir($od);
+		@chmod("$dir", 0777);
+		return @rmdir("$dir");
+	} else {
+		@chmod("$dir", 0777);
+		@unlink("$dir");
+	}
 }
--- install/inc/ver_tables.php
+++ install/inc/ver_tables.php
@@ -1,49 +1,50 @@
 <?php
 // 此脚本将缺失的表添加到数据库
-// 它也用于安装发动机
+// 它也用于安装引擎
 $tab = mysqli_query($db,'SHOW TABLES');
 while ($tables = mysqli_fetch_array($tab)) {
-    $_ver_table[$tables[0]] = 1;
+	$_ver_table[$tables[0]] = 1;
 }
 $k_sql = 0;
 $ok_sql = 0;
 $opdirtables = opendir(H . 'install/db_tables');
 while ($filetables = readdir($opdirtables)) {
-    if (preg_match('#\.sql$#i', $filetables)) {
-        $table_name = preg_replace('#\.sql$#i', null, $filetables);
-        if (!isset($_ver_table[$table_name])) {
-include_once check_replace(H.'sys/inc/sql_parser.php');
-            $sql = SQLParser::getQueriesFromFile(H . 'install/db_tables/' . $filetables);
-            for ($i = 0; $i < count($sql); $i++) {
-                $k_sql++; // 查询计数器（用于安装程序）
-                if (@mysqli_query($db,$sql[$i])) {
-                    $ok_sql++; // 成功查询计数器（用于安装程序）
-                }
-            }
-        }
-    }
+	if (preg_match('#\.sql$#i', $filetables)) {
+		$table_name = preg_replace('#\.sql$#i', null, $filetables);
+		if (!isset($_ver_table[$table_name])) {
+			include_once check_replace(H.'sys/inc/sql_parser.php');
+			$sql = SQLParser::getQueriesFromFile(H . 'install/db_tables/' . $filetables);
+			for ($i = 0; $i < count($sql); $i++) {
+				$k_sql++; // 查询计数器（用于安装程序）
+				if (@mysqli_query($db,$sql[$i])) {
+					$ok_sql++; // 成功查询计数器（用于安装程序）
+				}
+			}
+		}
+	}
 }
 closedir($opdirtables);
+
 if (!isset($install)) {
-    // 执行一次性查询
-    $opdirtables = opendir(H . 'install/update/');
-    while ($rd = readdir($opdirtables)) {
-        if (preg_match('#^\.#', $rd)) continue;
-        if (isset($set['update'][$rd])) continue;
-        if (preg_match('#\.sql$#i', $rd)) {
-            include_once H . 'sys/inc/sql_parser.php';
-            $sql = SQLParser::getQueriesFromFile(H . 'install/update/' . $rd);
-            for ($i = 0; $i < count($sql); $i++) {
-                mysqli_query($db,$sql[$i]);
-            }
-            $set['update'][$rd] = true;
-            $save_settings = true;
-        } elseif (preg_match('#\.php$#i', $rd)) {
-            include_once H . 'install/update/' . $rd;
-            $set['update'][$rd] = true;
-            $save_settings = true;
-        }
-    }
-    closedir($opdirtables);
-    if (isset($save_settings)) save_settings($set);
+	// 执行一次性查询
+	$opdirtables = opendir(H . 'install/update/');
+	while ($rd = readdir($opdirtables)) {
+		if (preg_match('#^\.#', $rd)) continue;
+		if (isset($set['update'][$rd])) continue;
+		if (preg_match('#\.sql$#i', $rd)) {
+			include_once H . 'sys/inc/sql_parser.php';
+			$sql = SQLParser::getQueriesFromFile(H . 'install/update/' . $rd);
+			for ($i = 0; $i < count($sql); $i++) {
+				mysqli_query($db,$sql[$i]);
+			}
+			$set['update'][$rd] = true;
+			$save_settings = true;
+		} elseif (preg_match('#\.php$#i', $rd)) {
+			include_once H . 'install/update/' . $rd;
+			$set['update'][$rd] = true;
+			$save_settings = true;
+		}
+	}
+	closedir($opdirtables);
+	if (isset($save_settings)) save_settings($set);
 }
