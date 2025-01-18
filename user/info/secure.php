e4867f06f1787d2f17b8a2f8b9a4f1c18848cff7
guguan123 <guguanming@outlook.com>
2025-01-18 21:16:34 +0800

    格式化代码

--- user/info/secure.php
+++ user/info/secure.php
@@ -1,4 +1,4 @@
-<?
+<?php
 include_once '../../sys/inc/start.php';
 include_once '../../sys/inc/compress.php';
 include_once '../../sys/inc/sess.php';
@@ -13,32 +13,35 @@ only_reg();
 $set['title']='安全';
 include_once '../../sys/inc/thead.php';
 title();
-if (isset($_POST['save'])){
-if (isset($_POST['pass']) && dbresult(dbquery("SELECT COUNT(*) FROM `user` WHERE `id` = $user[id] AND `pass` = '".shif($_POST['pass'])."' LIMIT 1"), 0)==1)
-{
-if (isset($_POST['pass1']) && isset($_POST['pass2']))
-{
-if ($_POST['pass1']==$_POST['pass2'])
-{
-if (strlen2($_POST['pass1'])<6)$err='出于安全原因，新密码不能短于6个字符';
-if (strlen2($_POST['pass1'])>32)$err='密码长度超过32个字符';
+
+if (isset($_POST['save'])) {
+    if (isset($_POST['pass']) && dbresult(dbquery("SELECT COUNT(*) FROM `user` WHERE `id` = $user[id] AND `pass` = '".shif($_POST['pass'])."' LIMIT 1"), 0) == 1) {
+        if (isset($_POST['pass1']) && isset($_POST['pass2'])) {
+            if ($_POST['pass1'] == $_POST['pass2']) {
+                if (strlen2($_POST['pass1']) < 6) $err = '出于安全原因，新密码不能短于6个字符';
+                if (strlen2($_POST['pass1']) > 32) $err = '密码长度超过32个字符';
+            } else {
+                $err='新密码与确认不符';
+            }
+        } else {
+            $err='输入新密码';
+        }
+    } else {
+        $err='旧密码不正确';
+    }
+
+    if (!isset($err)) {
+        dbquery("UPDATE `user` SET `pass` = '".shif($_POST['pass1'])."' WHERE `id` = '$user[id]' LIMIT 1");
+        setcookie('pass', cookie_encrypt($_POST['pass1'],$user['id']), time()+60*60*24*365);
+        $_SESSION['message'] = '密码更改成功';
+        header("Location: ?");
+        exit;
+    }
 }
-else $err='新密码与确认不符';
-}
-else $err='输入新密码';
-}
-else $err='旧密码不正确';
-if (!isset($err))
-{
-dbquery("UPDATE `user` SET `pass` = '".shif($_POST['pass1'])."' WHERE `id` = '$user[id]' LIMIT 1");
-setcookie('pass', cookie_encrypt($_POST['pass1'],$user['id']), time()+60*60*24*365);
-$_SESSION['message'] = '密码更改成功';
-header("Location: ?");
-exit;
-}
-}
+
 err();
 aut();
+
 echo "<div id='comments' class='menus'>";
 echo "<div class='webmenu'>";
 echo "<a href='/user/info/settings.php'>通用</a>";
