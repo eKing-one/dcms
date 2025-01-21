53152cae3259c45eb3384d3e58e06514ff1f722b
guguan123 <guguanming@outlook.com>
2025-01-22 02:54:23 +0800

    修复论坛搜索的一堆毛病

--- forum/inc/search_form.php
+++ forum/inc/search_form.php
@@ -1,9 +1,10 @@
+<form method='get' action='search.php'>
+文本:<br />
+<input type='text' name='search' value='<?php echo htmlentities(($_GET['search'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>'/><br />
+搜寻地点:<br />
+<select name='in'>
+<option value='all'>所有位置</option>
 <?php
-echo "<form method='post' action='?search={$passgen}'>";
-echo "文本:<br /><input type='text' name='text' value='" . htmlentities($searched['text'], ENT_QUOTES, 'UTF-8') . "' /><br />";
-echo "搜寻地点:<br /><select name='in'>";
-echo "<option value=''>所有位置</option>";
-
 $q = dbquery("SELECT `id`,`name` FROM `forum_f`" . ((!isset($user) || $user['level']==0) ? " WHERE `adm` = '0'" : null) . " ORDER BY `pos` ASC");
 while ($forums = dbassoc($q)) {
 	echo "<option value='f{$forums['id']}'" . (($searched['in']['m'] == 'f' && $searched['in']['id'] == $forums['id']) ? " selected='selected'" : null) . ">&gt;&gt; " . htmlspecialchars($forums['name']) . "</option>";
@@ -12,6 +13,7 @@ while ($forums = dbassoc($q)) {
 		echo "<option value='r{$razdels['id']}'" . (($searched['in']['m'] == 'r' && $searched['in']['id'] == $razdels['id']) ? " selected='selected'" : null) . ">&gt; " . htmlspecialchars($razdels['name']) . "</option>";
 	}
 }
-echo "</select><br />";
-echo "<input type='submit' value='开始搜索' /><br />";
-echo "</form>";
+?>
+</select><br />
+<input type='submit' value='开始搜索' /><br />
+</form>
