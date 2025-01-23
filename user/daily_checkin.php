907a06335df59082ffb4975d43346b146394a4c8
guguan123 <guguanming@outlook.com>
2025-01-24 00:31:54 +0800

    每日签到

--- user/daily_checkin.php
+++ user/daily_checkin.php
@@ -21,12 +21,39 @@ if (dbresult(dbquery("SELECT * FROM checkin_records WHERE user_id = '{$user['id'
 	echo "今天已经签到过啦";
 	echo "</div>";
 } else {
+	// 查询昨天是否签到
+	$yesterday = date('Y-m-d', strtotime('-1 day'));
+	$yesterday_checkin = dbresult(dbquery("SELECT * FROM checkin_records WHERE user_id = '{$user['id']}' AND DATE(checkin_date) = '$yesterday'"), 0);
+
+	if ($yesterday_checkin > 0) {
+		// 获取连续签到次数
+		$streak = dbresult(dbquery("SELECT streak FROM checkin_records WHERE user_id = '{$user['id']}'"), 0);
+		$streak++;
+	} else {
+		$streak = 1;
+	}
+
+	// 奖励逻辑
+	if ($streak > 30) {
+		$points = 500;
+		$coins = 10;
+		dbquery("UPDATE `user` SET `balls` = `balls` + $points, `money` = `money` + $coins WHERE `id` = '{$user['id']}' LIMIT 1");
+	} else {
+		$points = ($streak > 1) ? 300 : 200;
+		dbquery("UPDATE `user` SET `balls` = `balls` + $points WHERE `id` = '{$user['id']}' LIMIT 1");
+	}
+
 	// 插入签到记录
-	dbquery("UPDATE `user` SET `balls` = `balls` + 500 WHERE `id` = '{$user['id']}' LIMIT 1");
-	dbquery("INSERT INTO checkin_records (user_id, checkin_date) VALUES ('{$user['id']}', NOW())");
+	dbquery("INSERT INTO checkin_records (user_id, checkin_date, streak) VALUES ('{$user['id']}', NOW(), $streak) ON DUPLICATE KEY UPDATE checkin_date = VALUES(checkin_date), streak = $streak");
+
 	echo "<div class=\"mess\">";
-	echo "签到成功!";
-	echo "</div>";
+	if ($streak > 30) {
+		echo "连续签到 $streak 天，获得 $points 积分和 $coins 金币";
+	} elseif ($streak > 1) {
+		echo "连续签到 $streak 天，获得 $points 积分";
+	} else {
+		echo "签到成功，获得 $points 积分";
+	}
 }
 
 include_once '../sys/inc/tfoot.php';
