9bfaa8df81a6e6f46566739fbd374196468765b1
guguan123 <guguanming@outlook.com>
2025-01-23 17:50:03 +0800

    ... 不知道该说什么

--- install/db_tables/user_log.sql
+++ install/db_tables/user_log.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS `user_log` (
-  `id` int NOT NULL auto_increment,
-  `id_user` int NOT NULL ,
-  `method` set('1','0') NOT NULL default '0',
-  `date` TIMESTAMP NOT NULL,
-  `ip` VARCHAR(39) DEFAULT NULL,
-  `ua` varchar(128) default NULL,
-  `ban` set('1','0') NOT NULL default '0',
-  `checkin_date` DATE DEFAULT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `id_user` (`id_user`),
-  KEY `date` (`date`)
+	`id` int NOT NULL auto_increment,
+	`id_user` int NOT NULL ,
+	`method` set('1','0') NOT NULL default '0',
+	`date` TIMESTAMP NOT NULL,
+	`ip` VARCHAR(39) DEFAULT NULL,
+	`ua` varchar(128) default NULL,
+	`ban` set('1','0') NOT NULL default '0',
+	PRIMARY KEY  (`id`),
+	KEY `id_user` (`id_user`),
+	KEY `date` (`date`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
